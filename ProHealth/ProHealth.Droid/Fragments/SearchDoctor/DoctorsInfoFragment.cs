17ffa48a371536ee0cb49fe5f980a52a266ff904
SIVAMOHANREDDY <sivamohan.reddy@walkingtree.in>
2016-12-22 00:10:15 +0530

    Added SearchDoctor page and Profile page

---
+++ ProHealth/ProHealth.Droid/Fragments/SearchDoctor/DoctorsInfoFragment.cs
@@ -0,0 +1,60 @@
+
+using Android.App;
+using Android.OS;
+using Android.Support.V7.Widget;
+using Android.Views;
+using Android.Widget;
+using ProHealth.Droid.Adapters;
+using ProHealth.Droid.Helpers;
+
+namespace ProHealth.Droid.Fragments.SearchDoctor
+{
+    public class DoctorsInfoFragment : BaseFragment
+    {
+        protected override int FragmentId => Resource.Layout.DoctorsInfoView;
+        private RecyclerView recycler;
+        private RecyclerView.LayoutManager layoutMgr;
+        private Activity activity;
+        private DoctorsInfoAdapter blockAdapter;
+
+        private DoctorsInformation doctorsInformation;
+        private View view;
+
+        public DoctorsInformation Doctorsinformation
+        {
+            get
+            {
+                if (doctorsInformation == null)
+                    doctorsInformation = new DoctorsInformation();
+                return doctorsInformation;
+            }
+        }
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            view = base.OnCreateView(inflater, container, savedInstanceState);
+            recycler = initRecycler();
+            activity = this.Activity;
+            return view;
+        }
+
+        private RecyclerView initRecycler()
+        {
+            //Initialize the RecyclerView
+            recycler = view.FindViewById<RecyclerView>(Resource.Id.doctorsRecycler);
+            layoutMgr = new LinearLayoutManager(activity);
+            doctorsInformation = new DoctorsInformation();
+            blockAdapter = new DoctorsInfoAdapter(doctorsInformation, activity);
+            blockAdapter.ItemClick += OnItemClick;
+            recycler.SetLayoutManager(layoutMgr);
+            recycler.SetAdapter(blockAdapter);
+            return recycler;
+        }
+
+        public void OnItemClick(object sender, int e)
+        {
+            Toast.MakeText(activity, "item" + (e + 1), ToastLength.Short).Show();
+        }
+    }
+
+}
