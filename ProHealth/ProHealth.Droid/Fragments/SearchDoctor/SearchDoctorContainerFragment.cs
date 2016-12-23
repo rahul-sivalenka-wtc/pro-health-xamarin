c8b15a34b394cd6a0ca955acaf54cdd1ab56ac7d
Phani Rahul Sivalenka <rahul.sivalenka@walkingtree.tech>
2016-12-23 09:51:01 +0530

    Added searchdoctor view - Siva Fixed its UI

--- ProHealth/ProHealth.Droid/Fragments/SearchDoctor/SearchDoctorContainerFragment.cs
+++ ProHealth/ProHealth.Droid/Fragments/SearchDoctor/SearchDoctorContainerFragment.cs
@@ -1,7 +1,24 @@
+using Android.OS;
+using Android.Views;
+
 namespace ProHealth.Droid.Fragments.SearchDoctor
 {
     public class SearchDoctorContainerFragment : BaseFragment
     {
         protected override int FragmentId => Resource.Layout.SearchDoctorContainerView;
+
+        protected static int SearchDoctorRootFrame => Resource.Id.SearchDoctorRoot;
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            View view = base.OnCreateView(inflater, container, savedInstanceState);
+            return view;
+        }
+        public override void OnViewCreated(View view, Bundle savedInstanceState)
+        {
+            base.OnViewCreated(view, savedInstanceState);
+
+            GotoNextFragment<SearchDoctorListFragment>(SearchDoctorRootFrame);
+        }
     }
 }
